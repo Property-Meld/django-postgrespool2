be2daae55c01c34961b104d782405486e406a0ea
Austin Wentz <95113002+austin-wentz@users.noreply.github.com>
2024-03-01 13:44:24 -0700

    Ensure PostGIS init instantiated classes are imported

--- django_postgrespool2/postgis/base.py
+++ django_postgrespool2/postgis/base.py
@@ -1,4 +1,9 @@
 from django.contrib.gis.db.backends.postgis.base import *
 from django.contrib.gis.db.backends.postgis.base import DatabaseWrapper as Psycopg2DatabaseWrapper
+from django.contrib.gis.db.backends.postgis.schema import PostGISSchemaEditor
+from django.contrib.gis.db.backends.postgis.introspection import PostGISIntrospection
+from django.contrib.gis.db.backends.postgis.operations import PostGISOperations
+from django.contrib.gis.db.backends.postgis.features import DatabaseFeatures
 from django.db.backends.postgresql.creation import DatabaseCreation as Psycopg2DatabaseCreation
+
 from ..base import *
