48a985d3e9e9f6d183c7c31c40078e9ec9b5b700
Austin Wentz <95113002+austin-wentz@users.noreply.github.com>
2024-03-01 15:05:43 -0700

    Add import as for classes

--- django_postgrespool2/postgis/base.py
+++ django_postgrespool2/postgis/base.py
@@ -1,9 +1,9 @@
-from ..base import *
-
 from django.contrib.gis.db.backends.postgis.base import *
 from django.contrib.gis.db.backends.postgis.base import DatabaseWrapper as Psycopg2DatabaseWrapper
-from django.contrib.gis.db.backends.postgis.schema import PostGISSchemaEditor
-from django.contrib.gis.db.backends.postgis.introspection import PostGISIntrospection
-from django.contrib.gis.db.backends.postgis.operations import PostGISOperations
-from django.contrib.gis.db.backends.postgis.features import DatabaseFeatures
+from django.contrib.gis.db.backends.postgis.schema import PostGISSchemaEditor as DatabaseSchemaEditor
+from django.contrib.gis.db.backends.postgis.introspection import PostGISIntrospection as DatabaseIntrospection
+from django.contrib.gis.db.backends.postgis.operations import PostGISOperations as DatabaseOperations
+from django.contrib.gis.db.backends.postgis.features import DatabaseFeatures as DatabaseFeatures
 from django.db.backends.postgresql.creation import DatabaseCreation as Psycopg2DatabaseCreation
+
+from ..base import *
